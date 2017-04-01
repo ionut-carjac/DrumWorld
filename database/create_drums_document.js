476d0b3a08ad1d926cfd2015cd884bdb9033a53d
Ionut Carjac <carjac_ionut@yahoo.com>
2017-04-01 19:24:43 +0300

    Added drums documents

---
+++ database/create_drums_document.js
@@ -0,0 +1,218 @@
+db.drums.insert([
+{
+	name:"Mapex 14x5,5\" Special Panther Maple",
+	description:"Mapex 14\"x5,5\" Special Thomann Edition Black Panther Snare Drum, Finish: Transparent Walnut (WT), 100% Maplewood Shell, Chrome Hardware, thomann 2,3mm Power Hoops",
+	price:125,
+	producer:"Mapex",
+	stock:null,
+	sales:0
+},
+{
+	name:"Pearl MMP 14\"x5,5\" Snare Drum #165/B",
+	description:"Pearl MMP 14\"x5,5\" Snare Drum, Masters Premium Maple Series, MMP1455S/B #165, Colour: Diamond Burst.Black Shell Hardware, snare drum with 4-ply (5mm thick) maple shell and 5mm reinforcement rings",
+	price:439,
+	producer:"Pearl",
+	stock:null,
+	sales:0
+},
+{
+	name:"Yamaha 14x06 Rock Tour Snare Drum TSS",
+	description:"Yamaha Rock Tour Series 14\"x06\" Snare Drum, Color: Texture Smoke Sunburst (TSS), Rock Tour Texture Ash Finish, Variation thomann 6-Innerply Big Reaf Mahogany and 2-Outerply Ash Textured Shells with a rich and warm mid-low range tone.",
+	price:219,
+	producer:"Yamaha",
+	stock:null,
+	sales:0
+},
+{
+	name:"Millenium 22\"x14\" MX200 Series Bass Drum",
+	description:"Millenium 22\" x 14\" bass drum - covered shell with single-ply drum head from the Millenium MX200 series. thomann Colour: Black.",
+	price:66,
+	producer:"Millenium",
+	stock:null,
+	sales:0
+},
+{
+	name:"Tama 22\"x18\" Imperialstar BD -BK",
+	description:"Tama 22\"x18\" Bass Drum, Imperialstar Series, Colour Black (BK), 7,5mm thickness, 100% Poplarwood Shells, covered Shells, IMB22E-BK",
+	price:289,
+	producer:"Tama",
+	stock:null,
+	sales:0
+},
+{
+	name:"Sonor ProLite 16\"x16\" FT Red Tribal",
+	description:"Sonor ProLite 16\"x16\" Floor Tom, Color Red Tribal (-74), Version Exotic Veneer High Gloss lacquered Shell, chrome Shell thomann Hardware, 2,3 mm Power Hoops, Shell Construction: Vintage (Extra Thin), Shell Thickness: 9 layers = 4 mm + 2 mm Dynamic Edge, Tension Rods: 2 x 8 pieces, all Shells with a 45° bearing Edge in handselected extremly Thin 100% Maple Wood with Reinforcement thomann Rings for a open Sound with warm Tone and excellent balance between Mids and Hi Range, 3 Floortomlegs, all Lugs with Tunesafe, PL 12 1616 FT,",
+	price:679,
+	producer:"Sonor",
+	stock:null,
+	sales:0
+},
+{
+	name:"Millenium 16\"x16\" MX500 Series Floor Tom",
+	description:"Millenium MX500 Series 16 x 16\" floor tom - lacquered shell, single-ply drum head. Colour: Black",
+	price:45,
+	producer:"Millenium",
+	stock:null,
+	sales:0
+},
+{
+	name:"Gretsch 10\"x07\" TT Renown Maple -SB",
+	description:"Gretsch Renown Maple 10\"x07\" Tom Tom, RN-0710T-SB, matte lacquered Shell, Size: 10\"x07\", Color: Satin Black (SB), 6-ply with thomann 30° 100% All Maple Wood Shell, Gretsch Silver Sealer inside all drums, Die Cast Hoops, Evans Heads, GTS free Suspension Tom Mount System,",
+	price:199,
+	producer:"Gretsch",
+	stock:null,
+	sales:0
+},
+{
+	name:"Sonor ProLite 10\"x08\" TT Creme White",
+	description:"Sonor ProLite 10\"x08\" Tom Tom, Color Creme White (-70), Version Matte/Satin Semi Gloss lacquered Shells, chrome Shell Hardware, thomann 2,3 mm Power Hoops, Shell Construction: Vintage (Extra Thin), Shell Thickness: 9 layers = 4 mm + 2 mm Dynamic Edge, Tension Rods: 2 x 6 pieces, all Shells with a 45° bearing Edge in handselected extremly Thin 100% Maple Wood with Reinforcement Rings thomann for a open Sound with warm Tone and excellent balance between Mids and Hi Range, Tom Toms with T.A.R Mount System, all Lugs with Tunesafe, PL 12 1008 TT,",
+	price:344,
+	producer:"Sonor",
+	stock:null,
+	sales:0
+},
+{
+	name:"Sabian 14\" AAX X-Celerator Hi-Hat",
+	description:"Sabian AAX 14\" X-Celerator Hi-Hat - bright, ’Auto-Focus Response’ ensures crisp, accurate sounds, while ’airwave’ bottom eliminates air thomann lock for maximum clarity. Brilliant finish.",
+	price:311,
+	producer:"Sabian",
+	stock:null,
+	sales:0
+},
+{
+	name:"Zildjian 14\" ZXT Rock Hi-Hat",
+	description:"Zildjian ZXT Series, 14\" Rock Hi-Hat - hammered, clean and warm sounding cymbals.",
+	price:168,
+	producer:"Zildjian",
+	stock:null,
+	sales:0
+},
+{
+	name:"Zildjian 16\" A-Custom Crash",
+	description:"Zildjian 16\" A-Custom Crash - brilliant finish, natural and bright, beautiful warm undertones.",
+	price:205,
+	producer:"Zildjian",
+	stock:null,
+	sales:0
+},
+{
+	name:"Paiste 18\" Line Full Crash",
+	description:"Paiste Signature Line 18\" Full Crash Cymbal - bright, full, warm, brilliant sparkling. Wide range, balanced, clean mix. thomann Even response with warm, shimmering sustain. A very versatile, general purpose crash cymbal.",
+	price:355,
+	producer:"Paiste",
+	stock:null,
+	sales:0
+},
+{
+	name:"Zildjian 20\" A-Custom Projection Ride",
+	description:"Zildjian A-Custom 20\" Projection Ride - loud, clear bell and clean articulate ping with just the right amount of thomann shimmering undertones. Brilliant finish.",
+	price:268,
+	producer:"Zildjian",
+	stock:null,
+	sales:0
+},
+{
+	name:"Sabian 22\" HH Rock Ride",
+	description:"Sabian 22\" HH Hand Hammered Rock Ride - brilliant finish, powerful high end cut and warm tone combined with thomann bright bell for a loud, penetrating ride with character.",
+	price:277,
+	producer:"Sabian",
+	stock:null,
+	sales:0
+},
+{
+	name:"Sonor SP 473 Bass Drum Pedal",
+	description:"Sonor SP473 400 Series Bass Drum Pedal - aluminium design with support board.",
+	price:58,
+	producer:"Sonor",
+	stock:null,
+	sales:0
+},
+{
+	name:"Tama HP900PSWN PowerGlide IronCobra",
+	description:"Tama HP900PSWN, Power Glide System, Double Bass Drum Pedal, The Power Glide features a double-chained offset cam which thomann increases power and speed as the beater reaches the end of the stroke, new bearing Housing, Cobra Coil, Speedo-Ring, Oiles® bearing Hinge, Vari-Pitch Beater Holder, Felt Iron Cobra Beater, included Carrying Cases",
+	price:369,
+	producer:"Tama",
+	stock:null,
+	sales:0
+},
+{
+	name:"Tama 13\"x10\" Imperialstar Tom -HBK",
+	description:"Tama 13\"x10\" Tom Tom, Imperialstar Series, Colour: Hairline Black (HBK), 7,5mm thickness, 100% Poplarwood Shells, covered Shells, IMT13A-HBK,",
+	price:104,
+	producer:"Tama",
+	stock:null,
+	sales:0
+},
+{
+	name:"Sonor 13\"x10\" TT Essential Green Fad",
+	description:"Sonor Essential Force Series, 13\"x10\" Tom Tom, Color: Green Fade, highgloss lacquered Shell, Chrome Hardware, the 6-ply 100% thomann Birchwoodshells with 7,2mm thickness comes with a hefty and dynamic sound and with accentuated mid-ranges and highs, and defined lows, the hallmark of these shells, high-gloss-lacquered surfaces, Tuning Lugs with TuneSafe, vibration-free T.A.R. system (Total Acoustic ­Resonance) with APS provides maximum ­resonance and thomann sustain,",
+	price:123,
+	producer:"Sonor",
+	stock:null,
+	sales:0
+},
+{
+	name:"Pearl BC-900 Cymbal Boom Stand",
+	description:"Pearl BC-900 Cymbal Boom Stand - 900 Series, quickly changes from a boom stand to a straight cymbal stand, thomann Uni-Lock tilter, reversible wingnut, SureStrut double-braced legs, UltraGrip wingnuts/bolts.",
+	price:71,
+	producer:"Pearl",
+	stock:null,
+	sales:0
+},
+{
+	name:"Millenium CS-901 Pro Cymbal Stand",
+	description:"Millenium CS-901 Pro Series, straight double-braced cymbal stand, 2 adjustable memory clamps. Strong, heavy & professional construction.",
+	price:36,
+	producer:"Millenium",
+	stock:null,
+	sales:0
+},
+{
+	name:"Yamaha CS-650A Straight Cymbal Stand",
+	description:"Yamaha CS-650 - straight cymbal stand with 3x single braced legs, adjustable height (62-145cm).",
+	price:49,
+	producer:"Yamaha",
+	stock:null,
+	sales:0
+},
+{
+	name:"Vic Firth VFX5A Extreme 5A -Wood-",
+	description:"Vic Firth VFX5A Extreme 5A - wood tip, similar to a regular 5A stick but with more power and thomann reach. Length: 16.5\". Diameter: 0.57\".",
+	price:9.5,
+	producer:"Vic Firth",
+	stock:null,
+	sales:0
+},
+{
+	name:"Pro Mark TX5ABW Hickory -Wood",
+	description:"Pro Mark TX5ABW, Hickory Wood, oval Wood Tip, Length: 16\", Diameter: 0.551\"",
+	price:10.7,
+	producer:"Pro Mark",
+	stock:null,
+	sales:0
+},
+{
+	name:"Tama HS700WN Snare Stand",
+	description:"Tama HS700WN snarestand from the roadpro series, incl. omni-ball tilter, for 12\"-14\" snare drums, double braced leg, height: thomann 625-785mm",
+	price:79,
+	producer:"Tama",
+	stock:null,
+	sales:0
+},
+{
+	name:"Millenium DHS-1018 Stage Hi-Hat",
+	description:"Millenium DHS-1018 stage series Hi-Hat stand - 3x double braced legs, height adjustable from 69 cm to 96 cm",
+	price:29,
+	producer:"Millenium",
+	stock:null,
+	sales:0
+},
+{
+	name:"Pearl H-1000K Hi-Hat Stand Roller",
+	description:"Pearl H-1000K, Hi-Hat Stand, Super Pro Series, double Braced Hardware, swivelling Hi-Hat Pedal, spring tension adjustable, Ultra Grip thomann Screws, Eliminator Foot Plate, Roller Pulley Chain Drive System, for more comfortable feeling,",
+	price:158,
+	producer:"Pearl",
+	stock:null,
+	sales:0
+}
+]);
