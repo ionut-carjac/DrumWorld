734ddefdeed110b9f0989f74d7f70e8c5c8e6105
Ionut Carjac <carjac_ionut@yahoo.com>
2017-04-01 18:34:46 +0300

    MongoDB scripts - cymbals

---
+++ database/create_collections.js
@@ -0,0 +1,8 @@
+db.createCollection("admin");
+db.createCollection("comments");
+db.createCollection("cymbals");
+db.createCollection("drums");
+db.createCollection("hardware");
+db.createCollection("product_details");
+db.createCollection("sets");
+db.createCollection("users");
