beada1f585a43e239e0aaf80d4d30fd44bb8cd01
Ionut Carjac <ionut.carjac@pb.com>
2017-04-06 12:16:03 +0300

    Created setsDB  data file;

---
+++ database/create_sets_document.js
@@ -0,0 +1,162 @@
+use drums
+db.sets.insert([{
+		name:"Ludwig LC125-BK Accent CS Power Kit",
+		description:"Ludwig LC125-BK Accent CS Combo Power complete Set, Features a 22\"x16\" Bass Drum, 16\"x16\" Floor Tom, 12\"x09\" - thomann 13\"x10\" Tom Toms, 14\"x6,5\" Snare Drum, durable PVC finish in color Black, Shells feature 9-ply 8mm select Hardwood Veneers, the Combo Set comes with a 6-pcs lightweight, sturdy double braced hardware from the Ludwig 200 Series that contains Cymbalboomstand (L236MBS), straight Cymbalstand (L226CS), thomann Snare Stand (L222SS), Hi-Hat Stand (L216HH) included Drummerthrone (L247TH), also included a complete Brass Cymbal Set with 14\" Hi-Hat - 16\" Crash and 20\" Ride Cymbal in best Brass Starter Quality Material",
+		price:449,
+		producer:"Ludwig",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Millenium MX500",
+		description:"Millenium MX500 5-piece deluxe drum kit - high-gloss lacquered shells, 12\" & 13\" toms, 16\" floor tom, 22\" bass thomann drum, 14\" x 5 1/2\" wooden snare, double-braced hardware. Includes cymbal stand, cymbal boom stand, hi-hat stand, snare stand & bass drum pedal. Colour: High Gloss Black",
+		price:248,
+		producer:"Millenium",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Sonor Smart Force Black Stage 1",
+		description:"Sonor Smart Force Series, Stage 1 Model, Color: Black, Set containing 22\"x17,5\" Bass Drum (with Bracket), 12\"x09\" - thomann 13\"x10\" Tom Toms, 16\"x16\" Floor Tom, 14\"x5,5\" Snare Drum, DTH Doubletomholder, 4-pcs doublebraced Hardwarepack containing Hi-Hat Stand (HH174), Snare Stand (SS177), straight Cymbalstand (CS171), Bass Drum Pedal (SP273), 9-layer 7,2mm thickness Poplar Wood Shells, Sonor Designs Tune Safe Tuning Lugs, Ball Clamp Doubletomholder",
+		price:483,
+		producer:"Sonor",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Yamaha Gigmaker Standard Black",
+		description:"Yamaha Gigmaker Series standard version - covered basswood shells, 1.0mm x 7-ply (bass drum), 1.0mm x 6-ply (floor thomann toms, tom toms, snare drums), regular steel hoops, single-ply Yamaha heads, Yamaha double tom holder with omni ball system, includes hardware set (1x CS651W straight cymbal stand, HS651W hi-hat stand, SS650W snare stand and FP6110 bass drum pedal. Colour: Black. (Cymbals are not thomann included)",
+		price:542,
+		producer:"Yamaha",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Pearl FZ725Z/B Standard #31",
+		description:"Pearl FZ725Z/B Forum FZ Series Drum Kit - standard version, black shell hardware, covered shells, 6-ply 7.5mm poplar thomann shells, TH-70I Tom Tom holder, I.S.S. Tom Tom system. Hardware pack contains: H-72W Hi-Hat stand, S-70W snare stand, P-120P bass drum pedal, C-70W straight cymbal stand, B-70W cymbal boom stand, D-70W drum stool. included Zildjian Planet Z Cymbalset containing 20\" ride, 16\" crash, thomann 14\" Hi-Hat, \"Standard\" shell configuration: 22\" x 18\" bass drum, 12\" x 9\" & 13\" x 10\" toms, 16\" x 16\" floor tom and 14\" x 5.5\" wood snare drum. Colour: Jet Black (#31)",
+		price:666,
+		producer:"Pearl",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Sonor Ascent Creme White Stage 1",
+		description:"Sonor Ascent Series, Stage 1 Configuration, Color Creme White (-33), Black Chrome Shell Hardware, Set Configuration containing 22\"x18\" thomann Bass Drum, 12\"x09\" - 13\"x10\" Tom Toms, 16\"x16\" Floor Tom, 14\"x5,5 Snare Drum, 100% Beechwood Shells in 9 layers...",
+		price:1999,
+		producer:"Sonor",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Startone Star Drum Set Standard -BK",
+		description:"Startone Star Drum Set, 5-pcs Starter Drum Set containing 10\"x08\" - 12\"x09 Tom Toms, 16\"x16\" Floortom, 14\"x 5 1/2\" thomann Wood Snare Drum all Shells in 6-ply- and 22\"x14\" Bass Drum in 9-ply Poplarwood,doublebraced Hardwareset containing straight Cymbals...",
+		price:172,
+		producer:"Startone",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Millenium MX700 Stage 1 Pro Bundle BBL",
+		description:"Millenium MX700 Pro Stage 1 Drum Kit - 22\" x 18\" bass drum, 12\" x 9\" & 13\" x thomann 10\" toms, 16\" x 16\" floor tom and 14\" x 5-1/2\" wood snare. Free suspended rim mounting system for toms, 9-ply birch plywood shells. Includes double braced hardware set c...",
+		price:285,
+		producer:"Millenium",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Pearl TGB-625C Target Standard B#31",
+		description:"Pearl TGC-625C, Target Standard Drum Set, Color Black (#31), black hardware, Pearl's entry-level kit with professionell Features, complete thomann Set containing 22\"x16\" Bass Drum, 12\"x10\" - 13\"x11\" Tom Toms, 16\"x16\" Floortom, 14\"x5,5\" Wood Snare Drum, H...",
+		price:459,
+		producer:"Pearl",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Pearl TGC-625C Target Standard #33",
+		description:"Pearl TGC-625C, Target Standard Drum Set, Color White (#33), Pearl's entry-level kit with professionell Features, complete Set containing thomann 22\"x16\" Bass Drum, 12\"x10\" - 13\"x11\" Tom Toms, 16\"x16\" Floortom, 14\"x5,5\" Wood Snare Drum, Hi-Hat Stand H-62...",
+		price:459,
+		producer:"Pearl",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Ludwig LC125-BK Accent CS Power Kit",
+		description:"Ludwig LC125-BK Accent CS Combo Power complete Set, Features a 22\"x16\" Bass Drum, 16\"x16\" Floor Tom, 12\"x09\" - thomann 13\"x10\" Tom Toms, 14\"x6,5\" Snare Drum, durable PVC finish in color Black, Shells feature 9-ply 8mm select Hardwood Veneers, the Combo Se...",
+		price:469,
+		producer:"Ludwig",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Yamaha Gigmaker Studio Black B-Stock",
+		description:"B-Stock, Yamaha Gigmaker Series Standard Drum Kit - shell configuration: 20\" x 16\" bass drum, 10\" x 08\" tom thomann tom, 12\" x 09\" tom tom, 14\" x 14\" floor tom, 14\" x 5.5\" snare drum (wood), covered bass wood shells: 1.0mm x 7-ply (bass drum), 1.0mm x 6-ply (floor toms, tom toms, snare drums), regular steel hoops, single-ply Yamaha heads, Yamaha double thomann tom holder with omni-ball system. Includes hardware set - 1x CS651W straight cymbal stand, HS651W hi-hat stand, SS650W snare stand and FP6110 bass drum pedal. Colour: Black. (Cymbals are nor included), B-Stock with full warranty, may have slight traces of use",
+		price:508,
+		producer:"Yamaha",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Sonor Smart Force Brush Blue Stage 1",
+		description:"Sonor Smart Force Series, Stage 1 Model, Color: Brushed Blue, Set containing 22\"x17,5\" Bass Drum (with Bracket), 12\"x09\" thomann - 13\"x10\" Tom Toms, 16\"x16\" Floor Tom, 14\"x5,5\" Snare Drum, DTH Doubletomholder, 4-pcs doublebraced Hardwarepack containing Hi-Hat Stand (HH174), Snare Stand (SS177), straight Cymbalstand (CS171), Bass Drum Pedal (SP273), 9-layer 7,2mm thickness Poplar Wood Shells, Sonor Designs Tune Safe Tuning Lugs, Ball Clamp thomann Doubletomholder,",
+		price:532,
+		producer:"Sonor",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"DW PDP BX Standard SOB /Millenium",
+		description:"DW PDP BX Series complete Drum Set, Standard Version, Color: Solid Black, Black Shell Hardware. Set configuration: 22\"x18\" Bass Drum, 10\"x08\" - 12\"x09\" Tom Tom, 16\"x16\" Stand Toms, 14\"x5\" Snare Drum, included Millenium Hardwarepack HW-750 containing HH-750 Hi-Hat Stand, CS-750 straight Cymbal Stand, CB-750 Cymbal Boom Stand, SS-750 Snare Stand, PD-750 Single Drum Pedal, included Millenium HL3 Cymbalset containing 14\" Hi-Hat, 16\" Crash and 20\" Ride Cymbal and also included Millenium MDT3 Drummer Throne, DW PDP BX STANDARD SOLID BLACK (order code 235126), MILLENIUM PRO HW-750 HARDWARE PACK (order code 225762), MILLENIUM HL3 CYMBAL SET STANDARD (order code 107163), MILLENIUM MDT4 DRUM THRONE ROUND (order code 190868) Drums / Drum Sets ",
+		price:589,
+		producer:"DW",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Mapex HL5255-CY Standard Cherry",
+		description:"Mapex Horizon Series Model HL5255-CY, Set in Standard Version, Color: Transparent Cherry Red (CY), Set containing 22\"x18\" Bass thomann Drum, 12\"x09\" - 13\"x10\" Tom Toms, 16\"x16\" Floor Tom, 14\"x5,5\" Snare Drum, Bass Drum Pedal (P500), Snare Stand (S500), straight Cymbalstand (C500), Cymbalboom Stand (B500), Doubletomholder (TH657), Horizon Set features 7.2mm Birch Basswood mixed shell with lacquered finishes, New double tom holder with thomann auxiliary cymbal placement position, Re-engineered Isolated Tom Mount System (ITS), New tom bracket for quick set up and tear down, New Low-profile cushioned bass drum claws, New 500 series hardware, New Memory Locks For Extra Security on 500 series hardware, New Multi-Sustain Cymbal thomann Felts on 500 series hardware, New retractable Boom arm, Remo® equipped, inlcunding self-muffling bass drum heads",
+		price:675,
+		producer:"Mapex",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Gretsch G2 Series Standard Grey",
+		description:"Gretsch G2 Series complete Drumset, Configuration Standard / Fusion, Color: Grey Steel (GS), PVC Wraped covered, Chrome Shell thomann Hardware, Poplar Wood Shells, 30° Bearing Edge on Toms and 45° on Snare, 1,6mm Steel Hoops, Set containing 22\"x18\" Bass Drum, 14\"x5,5\" Snare Drum, 10\"x07\" - 12\"x08\" Tom Tom, 16\"x16\" Floor Tom, Snare Stand, Hi-Hat Stand, Cymbal Boom Stand, straight Cymbalstand, Bass Drum thomann Pedal also Sabian Sbr Brass Cymbal Set 14\" Hi-Hat - 16\" Crash - 20\" Ride,",
+		price:629,
+		producer:"Gretsch",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Yamaha Stage Custom Birch Std. NW",
+		description:"Yamaha Stage Custom Birch Series \"Standard\" Drum Kit - high quality lacquered shells, 100% birch wood. Powerful, warm, thomann dynamic and clear attack. Shell configuration: 22\" x 17\" bass drum, 10\" x 8\" & 12\" x 9\" toms, 16\" x 16\" floor tom, 14\" x 5.5\" BSD0655 wood snare. 700 single-braced hardware pack containing: 2x CS755 cymbal boom stands, 1x HS740A Hi-Hat stand, thomann 1x SS745A snare stand, 1x FP7210 single bass drum pedal. Colour: Natural Wood (NW)",
+		price:990,
+		producer:"Yamaha",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Tama SK52HXZB5-RCS",
+		description:"Tama SK52HXZB5-RCS, Superstar \"Hyper Indie-Pop\" Drum Set, Unicolor Series, Covered Version, Cover Color: Red Cameleon Sparkle (RCS), Hardware thomann Parts in Black Nickel Finish, Covered 100% Birch Shells, Bass Drum 7mm - 7 ply Shell, Toms-Floortoms-Snaredrum 6mm - 6 ply Shell, Die Cast Hoop with integrated Star Cast Tom Mount System on all mounted Shells, Bass Drum with Woodhoop in Setcolor, Snare thomann Drum with Die Cast Hoop, continuous Rubberplated \"Sound Bridge\" Hi-Tension Lugs with minimum Shell Contact, Rubberplated Bass Drum Guss Die Cast Claws, Set Configuration: 22\"x20\" Bass Drum without Bass Drum Holder, 10\"x6,5\" and 12\"x07\" Tom Tom, 16\"x14\" Floor Tom, 14\"x5,5\" Wood Snare Drum, thomann 2x MC69 Tomholder Combi Stand Clamp, included Hardwarepackage containing: Single Bass Drum Pedal, Hi-Hat Stand, Snare Stand, Cymbal Boom Stand, Straight Cymbal Stand,",
+		price:1050,
+		producer:"Tama",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Gretsch G2 Series Standard Red",
+		description:"Gretsch G2 Series complete Drumset, Configuration Standard / Fusion, Color: Dark Red Metallic (DR), PVC Wraped covered, Chrome thomann Shell Hardware, Poplar Wood Shells, 30\° Bearing Edge on Toms and 45° on Snare, 1,6mm Steel Hoops, Set containing 22\"x18\" Bass Drum, 14\"x5,5\" Snare Drum, 10\"x07\" - 12\"x08\" Tom Tom, 16\"x16\" Floor Tom, Snare Stand, Hi-Hat Stand, Cymbal Boom Stand, straight Cymbalstand, Bass thomann Drum Pedal also Sabian Sbr Brass Cymbal Set 14\" Hi-Hat - 16\" Crash - 20\" Ride,",
+		price:629,
+		producer:"Gretsch",
+		stock:null,
+		sales:0
+	},
+	{
+		name:"Millenium MX900 T-Drive Stage Birch -WS",
+		description:"Millenium MX900 T-Drive Stage Birch Set, 900 Series, 6-ply Bichwoodshells in highgloss lacquered Sparkle Finish, Color: White Sparkle thomann (#WS), Black Nickel Shell Hardware, Drumset included Satin Finish Hardware, Shellkonfiguration containing 22\"x18\" Bass Drum w/o Bracket, 14\"x6,5\" Snare Drum, 10\"x08\" - 12\"x09\" Tom Toms with Free Suspended Tom Mount System and 2x Tom Tom Holders, 14\"x12\" - 16\"x14\" Floor Toms, all Shells thomann with Remo UT Drumheads, included Hardwarepack in Satin Chrome Finish containing Snare Stand, Hi-Hat Stand, 2x Cymbal Boom Stand, Single Bass Drum Pedal and 2x Multiclamps, Set included also Sound control Set with 18\" Bass Drum Muffler cushion and Sound Control Ring Set thomann 10-12-14-14-16, Cymbals not included,",
+		price:379,
+		producer:"Millenium",
+		stock:null,
+		sales:0
+	}
+]);
