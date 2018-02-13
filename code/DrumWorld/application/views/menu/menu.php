f7129cbcdfffe563410eaccba2c50b4066795af7
Ionut Carjac <ionut.carjac@pb.com>
2018-02-13 17:18:13 +0200

    cleaned up some code

--- code/DrumWorld/application/views/menu/menu.php
+++ code/DrumWorld/application/views/menu/menu.php
@@ -1,18 +1,3 @@
-<!--
-<div class="sidebar-wrapper">
-    <ul class="sidebar-toggle">
-        <?php foreach ($menus as $menu) { ?>
-            <li style="font-size:50px"><a href="http://google.com"><?= $menu?></a></li>
-            
-        <?php } ?> 
-        <li style="font-size:50px"><a href="<?php echo base_url()?>index.php/drum/view/">Drums</a></li>
-    </ul>    
-</div>
-
-
-<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
--->
-
 <div class = "container" align = "center">
 	<nav class="navbar navbar-inverse">
 		<ul class="nav navbar-nav">
--- code/DrumWorld/application/views/pages/drum.php
+++ code/DrumWorld/application/views/pages/drum.php
@@ -2,19 +2,31 @@
 	TEST DRUM PAGE
 	<BR>
 	<BR>
-	<p><b>Consumer's Opinion</b></p>	
+	<p>
+		<b>Consumer's Opinion</b>
+	</p>
 	<BR>
 
 	<div style="width:400px; border:1px solid #632415; background-color: #2aabd2; padding:5px;">
 		<b> Leave a Comment:</b>
 		<hr size="1">
 		<form action="add_comment.php" method="POST">
-			<b>Name:</b>  <input type="text" name="user_name"><br>
-			<b>Email:</b> <input type="text" name="email"><br><br>
-			<b>Comment:</b> <br>
-			<textarea name="comment" cols="40"></textarea><br><br>
+			<b>Name:</b>
+			<input type="text" name="user_name">
+			<br>
+			<b>Email:</b>
+			<input type="text" name="email">
+			<br>
+			<br>
+			<b>Comment:</b>
+			<br>
+			<textarea name="comment" cols="40"></textarea>
+			<br>
+			<br>
 			<input type = "hidden" name = "category" value = "Drums">
-			<center><input type="submit" class = "btn btn-danger"value="Comment"></center>
+			<center>
+				<input type="submit" class = "btn btn-danger"value="Comment">
+			</center>
 		</form>
 	</div>
 	<BR>
